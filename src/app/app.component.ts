88c0d6dd9a0a3936a8f70dbb23caf19de89ec0e7
IvanTale <thefirst.ronin.ivan@gmail.com>
2021-11-01 23:34:25 +0200

    Reviewed bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string,
+  text: string,
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+  posts: Post[] = [
+    {title: 'Хочу выучить ангуляр компоненты', text: 'Я всё ещё учу ангуляр компоненты', id:1},
+    {title: 'Как я потратил 2 часа на непонятно что', text: 'Далее будет замес', id:2},
+  ]
+
+  updatePost(post: Post){
+    this.posts.unshift(post);
+    console.log('New post',post)
+  }
 }
